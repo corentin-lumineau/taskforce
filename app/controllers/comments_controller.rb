0305c5c783a2adb84c4071176123902f8858ab3b
Corentin Lumineau <lumineau.corentin@gmail.com>
2021-01-13 19:40:06 +0100

    Finalisation du front

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,16 @@
 class CommentsController < ApplicationController
 
     def create
-        binding.pry
         @task = Task.find(params[:task_id])
         @comment = Comment.new
         authorize @comment
         @comment.task_id = @task.id
         @comment.content = comment_params[:content]
         @comment.save!
+
+        if @task.save
+            redirect_to tasks_path
+        end
     end
 
     def new
